b53e3a573b1f7319f3b13aff4cf5cf511ca70d11
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-27 08:31:48 -0500

    added url import

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer();
 server.on('request', (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
   console.log('this is an incoming request');
 });
 
