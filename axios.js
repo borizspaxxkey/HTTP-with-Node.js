88649cd69778cef499cdd2a385349bcb080baf73
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-29 12:57:39 -0500

    completed

--- axios.js
+++ axios.js
@@ -5,18 +5,30 @@ axios({
   method: 'post',
   url: 'http://localhost:8080/users',
   data: {
-    userNames: ['danny', 'freedy']
+    userName: 'danny'
   },
   transformRequest: (data, headers) => {
-    const newData = data.userNames.map((userName) => {
-      return userName + '!';
-    });
+    const newData = {
+      userName: data.userName + '!'
+    }
     return JSON.stringify(newData);
   }
 })
   .then((response) => {
+    console.log('res', response.data);
     response.data.pipe(fs.createWriteStream('google.html'))
   })
   .catch((error) => {
     console.log(error)
   });
+
+const getMetadata = () => {
+  return axios.get('http://localhost:8080/metadata?id=1')
+}
+axios.all([
+  getMetadata(),
+  getMetadata()
+]).then((responseArray) => {
+  console.log('multiple', responseArray[0].data.description)
+  console.log('multiple', responseArray[1].data.description)
+});
