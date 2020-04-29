cb92c438e2b20f4ae67fec6ba31f455425a32a50
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-28 20:12:39 -0500

    working with the request body

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const http = require('http');
 const url = require('url');
+const services = require('./services')
 
 const server = http.createServer();
 server.on('request', (req, res) => {
   const parsedUrl = url.parse(req.url, true);
-  console.log('this is an incoming request');
+  if (req.method === 'GET' && parsedUrl.pathname === '/metadata') {
+    const { id } = parsedUrl.query;
+    const metadata = services.fetchImageMetadata(id);
+    console.log(req.headers);
+  };
+  const body = [];
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    const parsedJSON = JSON.parse(Buffer.concat(body));
+    const userName = parsedJSON[0]['userName'];
+    console.log(userName);
+    services.createUser(userName);
+  });
 });
 
 server.listen(8080);
