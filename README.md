7bea702270fc980f33a3ae3204450e0866ef2b65
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-29 07:43:58 -0500

    configuring formidable

--- README.md
+++ README.md
@@ -11,3 +11,21 @@
 
 1.  Memory Efficiency - we can operate on our data one piece at a time
 2.  Time Efficiency
+
+// 2 places to Handle Error
+
+1.  Request Stream
+2.  Reponse Stream
+
+// Common Errors
+
+1.  ECONNRESET
+2.  ETIMEDOUT
+
+// Formidable Package [for file upload]
+
+1.  Fast
+2.  Automatically Write to Disk
+3.  Low memory footPrint
+4.  Graceful error handling
+5.  Very High Test Coverage
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const textBody = require('body');        // TextBody
 const jsonBody = require('body/json');  // Json Data
 const formBody = require('body/form'); // Form Data
 const anyBody = require('body/any');  //  not sure the format of our incoming data
-const fs = require('fs');
+const fs = require('fs');            // Reading File Stream
+const formidable = require('formidable');  // For file Uploading
 
 const server = https.createServer({
   key: fs.readFileSync('./ssl/server.key'),
@@ -31,16 +32,41 @@ server.on('request', (req, res) => {
         res.end('This was served with https!');
       }
     });
+  } else if (req.method === 'POST' && parsedUrl.pathname === '/upload') {
+    const form = new formidable.IncomingForm({
+      uploadDir: __dirname,
+      keepExtensions: true,
+      multiples: true,
+      maxFileSize: 5 * 1024 * 1024, // size in bytes
+      encoding: 'utf-8',
+      maxFields: 20
+    });
+    form.parse(req, (err, fields, files) => {
+      // fields contain the key value pair on on form
+      // Files parameter will contain the files on the form
+      // console.log('\n fields:');
+      // console.log(fields);
+      // console.log('\n files:');
+      // console.log(files);
+      if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Error!');
+      }
+      res.statusCode = 200;
+      res.end('success');
+    });
   }
   else {
-    res.writeHead(404, {
-      'X-Powered-By': 'Node'
-    });
-    console.log('not forund url');
-    res.statusCode = 404;
-    res.setHeader('hello', 'world');
-    res.setHeader('Content-Type', 'application/json')
-    res.end();
+    fs.createReadStream('./index.html').pipe(res);
+    // res.writeHead(404, {
+    //   'X-Powered-By': 'Node'
+    // });
+    // console.log('not forund url');
+    // res.statusCode = 404;
+    // res.setHeader('hello', 'world');
+    // res.setHeader('Content-Type', 'application/json')
+    // res.end();
   }
 
 });
