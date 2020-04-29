212a4010d7e6127efabfd87556703a2e23afaf9d
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-29 12:34:07 -0500

    using array

--- axios.js
+++ axios.js
@@ -2,9 +2,17 @@ const axios = require('axios');
 const fs = require('fs');
 
 axios({
-  method: 'get',
-  url: 'http://www.google.com',
-  responseType: 'stream'
+  method: 'post',
+  url: 'http://localhost:8080/users',
+  data: {
+    userNames: ['danny', 'freedy']
+  },
+  transformRequest: (data, headers) => {
+    const newData = data.userNames.map((userName) => {
+      return userName + '!';
+    });
+    return JSON.stringify(newData);
+  }
 })
   .then((response) => {
     response.data.pipe(fs.createWriteStream('google.html'))
