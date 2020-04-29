05a3b8aed5590f8c7cc574a23722d1114dd2bcc2
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-29 06:39:10 -0500

    Responding with json

---
+++ README.md
@@ -0,0 +1,13 @@
+// 443 for https connections
+
+// Creating a Response
+
+1.  Set status code
+2.  Set headers
+3.  Write data
+4.  Close Connection
+
+// Benefit of Stream
+
+1.  Memory Efficiency - we can operate on our data one piece at a time
+2.  Time Efficiency
--- server.js
+++ server.js
@@ -1,28 +1,48 @@
-const http = require('http');
+const https = require('https');
 const url = require('url');
 const services = require('./services')
-const textBody = require('body');
-const jsonBody = require('body/json');
-const formBody = require('body/form');
-const anyBody = require('body/any');
+const textBody = require('body');        // TextBody
+const jsonBody = require('body/json');  // Json Data
+const formBody = require('body/form'); // Form Data
+const anyBody = require('body/any');  //  not sure the format of our incoming data
+const fs = require('fs');
 
-const server = http.createServer();
+const server = https.createServer({
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.crt')
+});
 server.on('request', (req, res) => {
   const parsedUrl = url.parse(req.url, true);
   if (req.method === 'GET' && parsedUrl.pathname === '/metadata') {
     const { id } = parsedUrl.query;
     const metadata = services.fetchImageMetadata(id);
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    const serializedJSON = JSON.stringify(metadata);
+    res.write(serializedJSON);
+    res.end();
     console.log(req.headers);
-  };
-  const body = [];
-  req.on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    const parsedJSON = JSON.parse(Buffer.concat(body));
-    const userName = parsedJSON[0]['userName'];
-    console.log(userName);
-    services.createUser(userName);
-  });
+  } else if (req.method === 'POST' && parsedUrl.pathname === '/users') {
+    jsonBody(req, res, (err, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        services.createUser(body.userName);
+        res.end('This was served with https!');
+      }
+    });
+  }
+  else {
+    res.writeHead(404, {
+      'X-Powered-By': 'Node'
+    });
+    console.log('not forund url');
+    res.statusCode = 404;
+    res.setHeader('hello', 'world');
+    res.setHeader('Content-Type', 'application/json')
+    res.end();
+  }
+
 });
 
-server.listen(8080);
+server.listen(443); 
